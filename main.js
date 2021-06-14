1ed979c93ca5f9f35d5d6ce4bec3cf12dfe8f3b7
TurnOffTheTV <pfilmsxtras@gmail.com>
2021-06-14 12:10:12 -0400

    added stone textures

--- main.js
+++ main.js
@@ -17,19 +17,25 @@ function preload(){
   images = {
     grass:loadImage('images/grass.png'),
     dirt:loadImage('images/dirt.png'),
+    stone:loadImage('images/stone.png'),
     cliff_b:loadImage('images/cliff-b.png'),
     cliff_t:loadImage('images/cliff-t.png'),
-    cliff_br:loadImage('images/cliff-br.png'),
-    cliff_bl:loadImage('images/cliff-bl.png'),
+    cliff_br_dirt:loadImage('images/cliff-br-dirt.png'),
+    cliff_bl_dirt:loadImage('images/cliff-bl-dirt.png'),
+    cliff_br_stone:loadImage('images/cliff-br-stone.png'),
+    cliff_bl_stone:loadImage('images/cliff-bl-stone.png'),
     cliff_tr:loadImage('images/cliff-tr.png'),
     cliff_tl:loadImage('images/cliff-tl.png'),
     cliff_r:loadImage('images/cliff-r.png'),
     cliff_l:loadImage('images/cliff-l.png'),
     cliff_t:loadImage('images/cliff-t.png'),
     cliff_b:loadImage('images/cliff-b.png'),
-    side_bl:loadImage('images/side-bl.png'),
-    side_b:loadImage('images/side-b.png'),
-    side_br:loadImage('images/side-br.png'),
+    side_bl_dirt:loadImage('images/side-bl-dirt.png'),
+    side_b_dirt:loadImage('images/side-b-dirt.png'),
+    side_br_dirt:loadImage('images/side-br-dirt.png'),
+    side_bl_stone:loadImage('images/side-bl-stone.png'),
+    side_b_stone:loadImage('images/side-b-stone.png'),
+    side_br_stone:loadImage('images/side-br-stone.png'),
     bg:loadImage('images/bg.png')
     };
   models = {};
@@ -54,13 +60,6 @@ function errorDisplay(errorMessage) {
 }
 
 function mapLoad() {
-  for(var j=0;j<height;j++){
-    for(var i=0;i<width;i++){
-      image(images.bg,i,j);
-      i+=127;
-    }
-    j+=127;
-  }
   image(images.cliff_tl,0,0);
   image(images.cliff_t,64,0);
   image(images.cliff_t,64*2,0);
@@ -93,10 +92,10 @@ function mapLoad() {
   image(images.grass,64*8,64*2);
   image(images.cliff_r,64*9,64*2);
 
-  image(images.cliff_bl,0,64*3);
+  image(images.cliff_bl_dirt,0,64*3);
   image(images.grass,64,64*3);
   image(images.grass,64*2,64*3);
-  image(images.cliff_br,64*3,64*3);
+  image(images.cliff_br_dirt,64*3,64*3);
   image(images.grass,64*4,64*3);
 
   image(images.grass,64*6,64*3);
@@ -116,38 +115,38 @@ function mapLoad() {
   image(images.grass,64*8,64*4);
   image(images.cliff_r,64*9,64*4);
 
-  image(images.side_bl,0,64*5);
-  image(images.side_b,64,64*5);
-  image(images.side_b,64*2,64*5);
-  image(images.side_br,64*3,64*5);
+  image(images.side_bl_dirt,0,64*5);
+  image(images.side_b_dirt,64,64*5);
+  image(images.side_b_dirt,64*2,64*5);
+  image(images.side_br_dirt,64*3,64*5);
   image(images.grass,64*4,64*5);
-  image(images.cliff_bl,64*5,64*5);
+  image(images.cliff_bl_stone,64*5,64*5);
   image(images.cliff_b,64*6,64*5);
   image(images.cliff_b,64*7,64*5);
   image(images.cliff_b,64*8,64*5);
-  image(images.cliff_br,64*9,64*5);
+  image(images.cliff_br_stone,64*9,64*5);
 
   image(images.grass,0,64*6);
   image(images.grass,64,64*6);
   image(images.grass,64*2,64*6);
   image(images.grass,64*3,64*6);
   image(images.grass,64*4,64*6);
-  image(images.dirt,64*5,64*6);
-  image(images.dirt,64*6,64*6);
-  image(images.dirt,64*7,64*6);
-  image(images.dirt,64*8,64*6);
-  image(images.dirt,64*9,64*6);
+  image(images.stone,64*5,64*6);
+  image(images.stone,64*6,64*6);
+  image(images.stone,64*7,64*6);
+  image(images.stone,64*8,64*6);
+  image(images.stone,64*9,64*6);
 
   image(images.grass,0,64*7);
   image(images.grass,64,64*7);
   image(images.grass,64*2,64*7);
   image(images.grass,64*3,64*7);
   image(images.grass,64*4,64*7);
-  image(images.side_bl,64*5,64*7);
-  image(images.side_b,64*6,64*7);
-  image(images.side_b,64*7,64*7);
-  image(images.side_b,64*8,64*7);
-  image(images.side_br,64*9,64*7);
+  image(images.side_bl_stone,64*5,64*7);
+  image(images.side_b_stone,64*6,64*7);
+  image(images.side_b_stone,64*7,64*7);
+  image(images.side_b_stone,64*8,64*7);
+  image(images.side_br_stone,64*9,64*7);
 
   image(images.grass,0,64*8);
   image(images.grass,64,64*8);
@@ -162,6 +161,13 @@ function mapLoad() {
 }
 
 function draw() {
+  for(var j=0;j<height;j++){
+    for(var i=0;i<width;i++){
+      image(images.bg,i,j);
+      i+=127;
+    }
+    j+=127;
+  }
   push();
   translate(cx,cy);
   mapLoad();
